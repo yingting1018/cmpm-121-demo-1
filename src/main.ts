e361c3671b5c76fa40932c61f3b4bbcb98c05651
yingting1018 <yhuan300@ucsc.edu>
2024-10-06 22:54:40 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,8 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const button = document.createElement('button');
+button.textContent = '🐱';
+app.append(button);
+console.log("meow")
