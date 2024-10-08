ab753b8d34025082708929e669fa48c7809fd060
yingting1018 <yhuan300@ucsc.edu>
2024-10-07 20:05:26 -0700

    Step 2 Complete

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,15 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-const button = document.createElement('button');
-button.textContent = '🐱';
+const button = document.createElement("button");
+button.textContent = "🐱";
 app.append(button);
-console.log("meow")
+
+const counterDisplay = document.createElement("div");
+counterDisplay.textContent = "Meow: 0"; 
+app.append(counterDisplay);
+let clickCount = 0;
+button.addEventListener("click", () => {
+    clickCount++;
+    counterDisplay.textContent = `Meows: ${clickCount}`;
+});
