d15a47a81a610f8baec7b3082edf9f623a83edd3
yingting1018 <yhuan300@ucsc.edu>
2024-10-10 16:54:54 -0700

    Step 6 Complete

--- src/main.ts
+++ src/main.ts
@@ -20,43 +20,93 @@ let clickCount = 0;
 
 button.addEventListener("click", () => {
   clickCount++;
-  // counterDisplay.textContent = `Meows: ${clickCount}`;
-  // updateShopButton();
 });
-// time();
-// function updateShopButton()
-// {
-//   shop.disabled = clickCount < 10;
-// }
+const shopA = document.createElement("button");
+shopA.textContent = "Good Luck 🍀";
+shopA.disabled = true;
+app.append(shopA);
 
-const shop = document.createElement("button");
-shop.textContent = "🍀";
-shop.disabled = true;
-app.append(shop);
+const shopB = document.createElement("button");
+shopB.textContent = "Super Luck 🍀";
+shopB.disabled = true;
+app.append(shopB);
 
-shop.addEventListener("click", () =>
-{
-  growthRate++;
+const shopC = document.createElement("button");
+shopC.textContent = "Amazing Luck 🍀";
+shopC.disabled = true;
+app.append(shopC);
+
+let shopACount = 0;
+let shopBCount = 0;
+let shopCCount = 0;
+
+const shopACountDisplay = document.createElement("div");
+shopACountDisplay.textContent = `Good Luck Purchases: ${shopACount}`;
+app.append(shopACountDisplay);
+
+const shopBCountDisplay = document.createElement("div");
+shopBCountDisplay.textContent = `Super Luck Purchases: ${shopBCount}`;
+app.append(shopBCountDisplay);
+
+const shopCCountDisplay = document.createElement("div");
+shopCCountDisplay.textContent = `Amazing Luck Purchases: ${shopCCount}`;
+app.append(shopCCountDisplay);
+
+shopA.addEventListener("click", () => {
+  growthRate+= 0.1;
   clickCount = clickCount - 10;
+  shopACount++;
+  growthRateDisplay.textContent = `Current Growth Rate: ${growthRate.toFixed(1)}`;
+  shopACountDisplay.textContent = `Good Luck Purchases: ${shopACount}`;
 });
-let growthRate = 1;
+shopB.addEventListener("click", () => {
+  growthRate = growthRate*2;
+  clickCount = clickCount - 100;
+  shopBCount++;
+  shopBCountDisplay.textContent = `Super Luck Purchases: ${shopBCount}`;
+  growthRateDisplay.textContent = `Current Growth Rate: ${growthRate.toFixed(1)}`;
+});
+shopC.addEventListener("click", () => {
+  growthRate = growthRate*50;
+  clickCount = clickCount - 1000;
+  shopCCount++;
+  shopCCountDisplay.textContent = `Amazing Luck Purchases: ${shopCCount}`;
+  growthRateDisplay.textContent = `Current Growth Rate: ${growthRate.toFixed(1)}`;
+});
 
+let growthRate = 1;
+const growthRateDisplay = document.createElement("div");
+growthRateDisplay.textContent = `Current Growth Rate: ${growthRate}`;
+app.append(growthRateDisplay);
+
+const PurchaseADisplay = document.createElement("div");
+PurchaseADisplay.textContent = `Current Growth Rate: ${growthRate}`;
 let previousTime: number = performance.now();
+
 function updateCount() {
-  if (clickCount >= 10)
-  {
-    shop.disabled = false;
+  if (clickCount >= 10) {
+    shopA.disabled = false;
   }
-  if (clickCount < 10)
-  {
-    shop.disabled = true;
+  if (clickCount >= 100) {
+    shopB.disabled = false;
+  }
+  if (clickCount >= 1000) {
+    shopC.disabled = false;
+  }
+  if (clickCount < 10) {
+    shopA.disabled = true;
+  }
+  if (clickCount < 100) {
+    shopB.disabled = true;
+  }
+  if (clickCount < 1000) {
+    shopC.disabled = true;
   }
   const currentTime = performance.now();
   const increment = currentTime - previousTime;
-  clickCount += increment / 1000 * growthRate;
+  clickCount += (increment / 1000) * growthRate;
   counterDisplay.innerHTML = `${Math.round(clickCount)} Meows`;
   previousTime = currentTime;
   requestAnimationFrame(updateCount);
 }
 requestAnimationFrame(updateCount);
-// updateShopButton();
